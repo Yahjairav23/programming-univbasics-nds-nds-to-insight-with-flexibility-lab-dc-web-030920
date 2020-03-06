5d4d0768d8464f21b51b031aff1a908185884575
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 09:07:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -96,10 +96,10 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  movies = {
-    :name => 
-    :movies => 
-  }
+  movies = []
+  index = 0 
+  
+  while 
   
 end
 

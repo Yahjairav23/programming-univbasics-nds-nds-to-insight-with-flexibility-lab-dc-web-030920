7534868ebee24b8d68b3a5b8809b475a4bd445ac
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 08:26:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -58,7 +58,7 @@ def movies_with_director_key(name, movies_collection)
   new_movies = []
   name_index = 0 
   while name_index < movies_collection.length do 
-    new_movies[name_index]
+    new_movies[name_index] = movies_collection[]
   
 end
 

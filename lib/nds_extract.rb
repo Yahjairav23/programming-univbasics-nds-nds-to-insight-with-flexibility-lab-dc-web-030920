bb0d30ddd5df69e947b3a2d21679a2434269db3e
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 08:18:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,6 +55,9 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   
+  new_movies = []
+  while movie_with_director_name(name, movies_collection)
+  
 end
 
 
